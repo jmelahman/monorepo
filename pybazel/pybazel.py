6290a2ef3ec2de28320fbec6e54970bfc12101de
Jamison Lahman <jamison@lahman.dev>
2022-05-27 20:57:00 -0700

    pybazel v0.1

---
+++ pybazel/pybazel.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import os
+import subprocess
+
+class BazelClient():
+    def __init__(self, workspace_path: str = "", bazel_options: list[str] = None) -> None:
+        self.bazel_options = bazel_options or []
+        self.workspace_path = workspace_path or os.environ["BUILD_WORKSPACE_DIRECTORY"]
+        self._bazel_bin = "bazel"
+
+    def sync(sync_options, check = True):
+        cmd = [
+            self._bazel_bin,
+            *self.bazel_options,
+            "sync",
+            *sync_options,
+        ]
+        return subprocess.check_call(cmd, cwd=self.workspace_path)
