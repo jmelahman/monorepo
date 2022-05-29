4b8542d89300879afe87fb1510617b119fdc69f8
Jamison Lahman <jamison@lahman.dev>
2022-05-29 03:07:33 -0700

    arch upload workflow

--- docker/arch/build.sh
+++ docker/arch/build.sh
@@ -3,7 +3,21 @@
 set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPOSITORY="lahmanja/arch"
+VERSION="v1.0.0"
+PUSH="false"
+
+if [ "${1:-}" == "--push" ]; then
+  PUSH="true"
+elif [ "${1:-}" == "--help" ]; then
+  echo "$(basename $0) usage: [--help] [--push]"
+  exit 0
+fi
 
 pushd "$SCRIPT_DIR" > /dev/null
 
-docker build -t lahmanja/dev:arch .
+docker build -t ${REPOSITORY}:latest -t ${REPOSITORY}:${VERSION} .
+if [ "${PUSH}" == "true" ]; then
+  docker login
+  docker push ${REPOSITORY} --all-tags
+fi
