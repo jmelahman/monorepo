03392330a7d2b62132f6245d3d2c21fb832629b5
Jamison Lahman <jamison@lahman.dev>
2022-05-28 20:42:05 -0700

    format test WIP

--- pybazel.py
+++ pybazel.py
@@ -12,9 +12,22 @@ class BazelClient:
         self.workspace_path = workspace_path or os.environ["BUILD_WORKSPACE_DIRECTORY"]
         self._bazel_bin = "bazel"
 
+    def run(
+        self, target: str, run_options: list[str] | None = None, check = True
+    ) -> subproccess.CompletedProcess:
+        run_options = run_options or []
+        cmd = [
+            self._bazel_bin,
+            *self.bazel_options,
+            "run",
+            *run_options,
+        ]
+        return subprocess.run(cmd, cwd=self.workspace_path, check=check)
+
     def sync(
-        self, sync_options: list[str], check: bool = True
+        self, sync_options: list[str] | None = None, check: bool = True
     ) -> subprocess.CompletedProcess:
+        sync_options = sync_options or []
         cmd = [
             self._bazel_bin,
             *self.bazel_options,
