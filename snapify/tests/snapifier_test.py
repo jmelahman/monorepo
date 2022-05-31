0b3535b2d8acbfe3f68555a275da0a988d3313cb
Jamison Lahman <jamison@lahman.dev>
2022-05-31 17:52:08 -0400

    refactor and rename

--- snapify/tests/snapifier_test.py
+++ snapify/tests/snapifier_test.py
@@ -1,11 +1,9 @@
-from __future__ import annotations
-
 import os
 from typing import Any
 import unittest
 from unittest import mock
 
-from snapify.pysnapify import main, constants
+from snapify.pysnapify import constants, snapifier
 from snapify.tests.testdata import names, os_release, user_config
 
 
@@ -41,56 +39,50 @@ class SnapifyTest(unittest.TestCase):
             with self.subTest(distro=expected_distro), mock.patch(
                 "builtins.open", mock.mock_open(read_data=release_data)
             ):
-                distro = main.Snapifier._check_supported_distro()
+                distro = snapifier.Snapifier._check_supported_distro()
                 self.assertEqual(distro, expected_distro)
 
     @mock.patch("os.path.exists", return_value=False)
     def test_no_user_config(self, mock_path_exists: mock.MagicMock) -> None:
-        main.Snapifier._read_user_config()
+        snapifier.Snapifier._read_user_config()
 
     @mock.patch("os.path.exists", return_value=True)
     def test_basic_user_config(self, mock_path_exists: mock.MagicMock) -> None:
         with mock.patch(
             "builtins.open", mock.mock_open(read_data=user_config.ARCH_IGNORE_DOCKER)
         ):
-            config = main.Snapifier._read_user_config()
+            config = snapifier.Snapifier._read_user_config()
             self.assertEqual(config, {constants.SupportedDistro.ARCH: ["docker"]})
 
-    @mock.patch("builtins.open", new=mock_open_arch)
-    @mock.patch("snapify.pysnapify.manager.snap.Snapd.get_installed_packages", return_value=[])
+    @mock.patch(
+        "snapify.pysnapify.manager.snap.Snapd.get_installed_packages", return_value=[]
+    )
     @mock.patch("snapify.pysnapify.manager.utils.get_executable")
     @mock.patch("os.path.exists")
-    def test_snapifier_arch(
+    def test_snapifier(
         self,
         mock_path_exists: mock.MagicMock,
         mock_get_executable: mock.MagicMock,
         mock_installed_snaps: mock.MagicMock,
     ) -> None:
+        subtests = [
+            (mock_open_arch, constants.SupportedDistro.ARCH),
+            (mock_open_manjaro, constants.SupportedDistro.MANJARO),
+        ]
         mock_path_exists.side_effect = [
             True,  # ~/.config/snapify/config
             True,  # /var/cache/snapd/names
-        ]
-        snapifier = main.Snapifier(noninteractive=False)
-        self.assertEqual(snapifier._distro, constants.SupportedDistro.ARCH)
-        self.assertEqual(snapifier._config, {constants.SupportedDistro.ARCH: ["docker"]})
+        ] * len(subtests)
 
-    @mock.patch("builtins.open", new=mock_open_manjaro)
-    @mock.patch("snapify.pysnapify.manager.snap.Snapd.get_installed_packages", return_value=[])
-    @mock.patch("snapify.pysnapify.manager.utils.get_executable")
-    @mock.patch("os.path.exists")
-    def test_snapifier_manjaro(
-        self,
-        mock_path_exists: mock.MagicMock,
-        mock_get_executable: mock.MagicMock,
-        mock_installed_snaps: mock.MagicMock,
-    ) -> None:
-        mock_path_exists.side_effect = [
-            True,  # ~/.config/snapify/config
-            True,  # /var/cache/snapd/names
-        ]
-        snapifier = main.Snapifier(noninteractive=False)
-        self.assertEqual(snapifier._distro, constants.SupportedDistro.MANJARO)
-        self.assertEqual(snapifier._config, {constants.SupportedDistro.ARCH: ["docker"]})
+        for mock_open, expected_distro in subtests:
+            with self.subTest(expected_distro=expected_distro.value), mock.patch(
+                "builtins.open", side_effect = mock_open
+            ):
+                snapify = snapifier.Snapifier(noninteractive=False)
+                self.assertEqual(snapify._distro, expected_distro)
+                self.assertEqual(
+                    snapify._config, {constants.SupportedDistro.ARCH: ["docker"]}
+                )
 
 
 if __name__ == "__main__":
