83bfd2f0e9c9346dca94c245cfd21eeaab96fd2b
Jamison Lahman <jamison@lahman.dev>
2022-05-31 19:17:45 -0400

    arch user docker

---
+++ docker/arch/start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+_REPO_ROOT=$( cd -- "$( dirname -- $( realpath "${BASH_SOURCE[0]}" ) )/../.." &> /dev/null && pwd )
+
+trap 'docker stop archdev && docker rm archdev' EXIT
+docker run \
+  -it \
+  -e "USER=$(id -un)" \
+  -u "$(id -u)" \
+  -v "${HOME}/.cache":"${HOME}/.cache" \
+  -v "${_REPO_ROOT}":"${_REPO_ROOT}" \
+  -w "${_REPO_ROOT}" \
+  --name archdev \
+  lahmanja/arch:v1.1.0 \
+  /usr/bin/bash
