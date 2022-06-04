5017fcaaf629183b011f70835899f2d46f15bfa3
Jamison Lahman <jamison@lahman.dev>
2022-06-03 21:32:31 -0400

    snap agnostic snap_test

--- snapify/tests/snap_test.py
+++ snapify/tests/snap_test.py
@@ -10,16 +10,24 @@ core                               16-2.55.5                   13250  latest/sta
 core18                             20220428                    2409   latest/stable     canonical    base
 core20                             20220512                    1494   latest/stable     canonical    base
 """
+_PACMAN_BIN = "/usr/bin/fake-pacman"
+_SUDO_BIN = "/usr/bin/fake-sudo"
 
 
 class SnapTest(unittest.TestCase):
     @mock.patch("subprocess.check_output", return_value=_SNAP_LIST)
+    @mock.patch("snapify.pysnapify.manager.utils.get_executable")
     @mock.patch(
         "snapify.pysnapify.manager.snap.Snapd._names_exists", return_value=False
     )
     def setUp(
-        self, mock_names_exists: mock.MagicMock, mock_subprocess: mock.MagicMock
+        self, mock_names_exists: mock.MagicMock, mock_get_executable: mock.MagicMock, mock_subprocess: mock.MagicMock
     ) -> None:
+        self._default_get_executable = [
+            _PACMAN_BIN,
+            _SUDO_BIN,
+        ]
+        mock_get_executable.side_effect = self._default_get_executable
         self.snap = snap.Snapd(noninteractive=False, ignored_packages=[])
 
     def test_get_installed(self) -> None:
@@ -29,9 +37,8 @@ class SnapTest(unittest.TestCase):
 
     @mock.patch("snapify.pysnapify.manager.snap.Snapd._names_exists", return_value=True)
     def test_get_available(self, mock_names_exists: mock.MagicMock) -> None:
-        available_packages = self.snap.get_available_packages()
         with mock.patch("builtins.open", mock.mock_open(read_data=names.NAMES)):
-            pass
+            available_packages = self.snap.get_available_packages()
             # self.assertIn("fake-snapify", available_packages)
 
 
