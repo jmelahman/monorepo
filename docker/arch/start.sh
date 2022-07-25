e3cd3cdc1f4cf50c7bf7fcee30fe9d0f78e59977
Jamison Lahman <jamison@lahman.dev>
2022-07-24 19:27:19 -0700

    hardcode repo location in docker

--- docker/arch/start.sh
+++ docker/arch/start.sh
@@ -4,9 +4,10 @@ _REPO_ROOT=$( cd -- "$( dirname -- "$( realpath "${BASH_SOURCE[0]}" )" )/../.." 
 _CONTAINER_NAME="archdev"
 
 trap 'docker stop ${_CONTAINER_NAME} && docker rm ${_CONTAINER_NAME}' EXIT
+USER="$(id -un)"
 docker run \
   -it \
-  -e "USER=$(id -un)" \
+  -e "USER=$USER" \
   -u "$(id -u)" \
   -v "${HOME}/.cache":"${HOME}/.cache" \
   -v "${HOME}/.ssh":"${HOME}/.ssh" \
@@ -14,7 +15,7 @@ docker run \
   -v "${_REPO_ROOT}/dotfiles/.gitconfig":"${HOME}/.gitconfig" \
   -v "${_REPO_ROOT}/dotfiles/.vim":"${HOME}/.vim" \
   -v "${_REPO_ROOT}/dotfiles/.vimrc":"${HOME}/.vimrc" \
-  -v "${_REPO_ROOT}":"${_REPO_ROOT}" \
+  -v "${_REPO_ROOT}":"/home/${USER}/code/monorepo" \
   -w "${_REPO_ROOT}" \
   --name ${_CONTAINER_NAME} \
   lahmanja/arch:latest \
