6ec9aa6a1bb2b78efe87e8619033fc7ab7defc1e
Jamison Lahman <jamison@lahman.dev>
2023-01-08 07:07:32 -0800

    running; git commit -m running

--- buildprint/buildprint/__main__.py
+++ buildprint/buildprint/__main__.py
@@ -1,5 +1,23 @@
+import time
+
+from buildprint.version import version, version_info
+
+
+def fib(n: int) -> int:
+    if n <= 1:
+        return n
+    else:
+        return fib(n - 2) + fib(n - 1)
+
+
+t0 = time.time()
+fib(32)
+print(time.time() - t0)
+
+
 def main() -> int:
-    print("hello")
+    print(version)
+    print(version_info)
     return 0
 
 
