d3d6269efa24cd5941afd3aa32ed588a0592e1f4
Jamison Lahman <jamison@lahman.dev>
2023-01-09 01:47:59 -0800

    maybe clean issue

--- pybazel/setup.py
+++ pybazel/setup.py
@@ -8,13 +8,14 @@ from mypyc.build import mypycify  # type: ignore[import]
 from setuptools import find_packages  # type: ignore[import]
 from setuptools import setup
 
+from pybazel import __title__
 from pybazel import __version__
 
 README = (pathlib.Path(__file__).parent / "README.md").read_text()
-NAME = "pybazel"
+__title__ = "pybazel"
 
 # Adopted from https://github.com/python/mypy/blob/master/setup.py
-def find_package_data(base, globs, root=NAME):
+def find_package_data(base, globs, root=__title__):
     """Find all interesting data files, for setup(package_data=)
     Arguments:
       root:  The directory to search in.
@@ -34,22 +35,22 @@ def find_package_data(base, globs, root=NAME):
 
 
 setup(
-    name=NAME,
+    __title__=__title__,
     version=__version__,
     description="A python client for Bazel",
     author="Jamison Lahman",
     author_email="jamison@lahman.dev",
     long_description=README,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/jmelahman/{NAME}",
+    url=f"https://github.com/jmelahman/{__title__}",
     py_modules=[],
     ext_modules=mypycify(
-        [os.path.join(NAME, x) for x in find_package_data(NAME, ["*.py"])]
+        [os.path.join(__title__, x) for x in find_package_data(__title__, ["*.py"])]
     ),
     keywords=["bazel", "bazelbuild", "buildtools", "tools"],
-    package_dir={NAME: NAME},
+    package_dir={__title__: __title__},
     packages=find_packages(),
-    download_url=f"https://github.com/jmelahman/{NAME}/archive/refs/tags/v{__version__}.tar.gz",
+    download_url=f"https://github.com/jmelahman/{__title__}/archive/refs/tags/v{__version__}.tar.gz",
     license="MIT",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -61,6 +62,6 @@ setup(
     ],
     install_requires=[
         "colorama==0.4.5",
-        "mypy==0.943",
+        "mypy==0.942",
     ],
 )
