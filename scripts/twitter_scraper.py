9ab3f917525acb4aa4310b380b029ac98c8a79e8
Jamison Lahman <jamison@lahman.dev>
2023-01-22 01:45:27 -0800

    twitter scraper

---
+++ scripts/twitter_scraper.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import io
+import os
+
+import tweepy
+
+
+def write_tweets(tweets_file: io.TextIOWrapper, tweets: tweepy.Tweet) -> None:
+    for tweet in tweets:
+        if not tweet.text or tweet.text.startswith("@") or tweet.text.startswith("RT"):
+            continue
+        tweets_file.write(tweet.text + "\n")
+
+
+def main() -> None:
+    client = tweepy.Client(os.environ["ACCESS_TOKEN"])
+    user = client.get_user(username="jmelahman")
+    tweets = client.get_users_tweets(user.data.id)
+    with open("my_tweets.txt", "w+") as my_tweets:
+        write_tweets(my_tweets, tweets)
+        while tweets.meta.get("next_token"):
+            tweets = client.get_users_tweets(
+                user.data.id, pagination_token=tweets.meta["next_token"]
+            )
+            write_tweets(my_tweets, tweets.data)
+
+
+if __name__ == "__main__":
+    main()
