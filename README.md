1189dc5ddc63866073f74caaf58146286100cf7c
Jamison Lahman <jamison@lahman.dev>
2023-04-30 22:35:36 -0700

    0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# check-symlinks
-Check for broken symlinks
+# Check Symlinks
+
+Checks for broken symbolic links.
+
+## Install
+
+```shell
+cargo install check-symlinks
+```
+
+## Usage
+
+It currently only supports checking all files recursively from your current working directory,
+
+```shell
+$ check-symlinks
+"./testdata/broken_link" is not a valid symlink
+```
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use std::fs;
+use std::io::Error;
+use std::path::{Path, PathBuf};
+use std::process::ExitCode;
+use std::sync::mpsc::channel;
+use threadpool::ThreadPool;
+use walkdir::WalkDir;
+
+fn is_valid_symlink<P: AsRef<std::path::Path>>(path: &P) -> Result<bool, Error> {
+    if let Ok(target_path) = fs::read_link(path) {
+        if let Ok(_) = fs::metadata(&target_path) {
+            return Ok(true);
+        }
+        let dirname = path.as_ref().parent().unwrap_or(Path::new(""));
+        if let Ok(_) = std::fs::metadata(PathBuf::from(dirname).join(target_path)) {
+            return Ok(true);
+        }
+    }
+    println!("{:?} is not a valid symlink", path.as_ref());
+    Ok(false)
+}
+
+fn lint<P: AsRef<std::path::Path>>(filepath: P) -> Result<bool, Error> {
+    if let Ok(metadata) = fs::symlink_metadata(&filepath) {
+        if metadata.file_type().is_symlink() {
+            return is_valid_symlink(&filepath);
+        }
+    }
+    Ok(true)
+}
+
+fn main() -> ExitCode {
+    let mut exit_code = 0;
+    let pool = ThreadPool::new(num_cpus::get());
+
+    let (tx, rx) = channel();
+
+    for entry in WalkDir::new(".")
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| !e.path().is_dir())
+    {
+        let path = entry.path().to_owned();
+        let tx = tx.clone();
+        pool.execute(move || {
+            let errors = lint(path);
+            tx.send(errors).expect("Could not send data!");
+        });
+    }
+    drop(tx);
+    for t in rx.iter() {
+        let Ok(passed) = t else {
+            todo!();
+        };
+        if !passed {
+            exit_code = 1;
+        }
+    }
+    ExitCode::from(exit_code)
+}
