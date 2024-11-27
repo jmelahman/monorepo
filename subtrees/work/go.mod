f136a1fcba175524de9686f20211f9c9aeaa9063
Jamison Lahman <jamison@lahman.dev>
2024-11-27 11:28:48 -0800

    refactor and working on autocomplete

--- subtrees/work/go.mod
+++ subtrees/work/go.mod
@@ -4,6 +4,7 @@ go 1.23.2
 
 require (
 	github.com/jessevdk/go-flags v1.6.1
+	github.com/posener/complete/v2 v2.1.0
 	modernc.org/sqlite v1.34.1
 )
 
@@ -13,6 +14,7 @@ require (
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/posener/script v1.2.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
--- subtrees/work/work.go
+++ subtrees/work/work.go
@@ -7,11 +7,13 @@ import (
 
 	"database/sql"
 	"github.com/jessevdk/go-flags"
+	"github.com/posener/complete/v2"
+	"github.com/posener/complete/v2/install"
+	"github.com/posener/complete/v2/predict"
 	_ "modernc.org/sqlite"
 )
 
 type Options struct {
-	Verbose bool `short:"v" long:"verbose" description:"Enable verbose output"`
 }
 
 type ClockInCommand struct {
@@ -24,6 +26,150 @@ type ReportCommand struct {
 }
 
 type StatusCommand struct {
+	Quiet bool `short:"q" long:"quiet" description:"Exit with status code"`
+}
+
+func handleClockIn() {
+	db, err := initializeDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`SELECT id, end FROM shift ORDER BY end DESC LIMIT 1`)
+	if err != nil {
+		panic(err)
+	}
+	if !rows.Next() {
+		rows.Close()
+		now := time.Now()
+		_, err := db.Exec(`INSERT INTO shift (id, start, end) VALUES (1, ?, ?)`,
+			now.Format(time.UnixDate),
+			now.Add(8*time.Hour).Format(time.UnixDate),
+		)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		var (
+			id  int64
+			end string
+		)
+		if err = rows.Scan(&id, &end); err != nil {
+			panic(err)
+		}
+		rows.Close()
+		now := time.Now()
+		endTime, err := time.Parse(time.UnixDate, end)
+		if err != nil {
+			panic(err)
+		}
+		if time.Until(endTime) < 0 {
+			_, err := db.Exec(`INSERT INTO shift (id, start, end) VALUES (?, ?, ?)`,
+				id+1,
+				now.Format(time.UnixDate),
+				now.Add(8*time.Hour).Format(time.UnixDate),
+			)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func handleClockOut() {
+	db, err := initializeDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`SELECT id, end FROM shift ORDER BY end DESC LIMIT 1`)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	if rows.Next() {
+		var (
+			id  int64
+			end string
+		)
+		if err = rows.Scan(&id, &end); err != nil {
+			panic(err)
+		}
+		rows.Close()
+		endTime, err := time.Parse(time.UnixDate, end)
+		if err != nil {
+			panic(err)
+		}
+		if time.Until(endTime) > 0 {
+			_, err := db.Exec(`UPDATE shift SET end=? WHERE id=?`, time.Now().Format(time.UnixDate), id)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func handleReport() {
+	db, err := initializeDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	_, err = db.Query(`SELECT start, end FROM shift ORDER BY end DESC`)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handleStatus(status StatusCommand) (int, error) {
+	db, err := initializeDB()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`SELECT end FROM shift ORDER BY end DESC LIMIT 1`)
+	if err != nil {
+		panic(err)
+	}
+	if rows.Next() {
+		var end string
+		if err = rows.Scan(&end); err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+		endTime, err := time.Parse(time.UnixDate, end)
+		if err != nil {
+			panic(err)
+		}
+		if time.Until(endTime) > 0 {
+			if status.Quiet != true {
+				fmt.Printf("Hours left: %v\n", endTime.Format(time.TimeOnly))
+			}
+			return 0, nil
+		}
+	}
+	if status.Quiet != true {
+		fmt.Printf("Not clocked in.")
+		return 0, nil
+	}
+	return 1, nil
+}
+
+func initializeDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite", "./example.db")
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS shift (id INTEGER PRIMARY KEY, start TIME, end TIME)`)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
 
 func main() {
@@ -34,134 +180,48 @@ func main() {
 	var status StatusCommand
 
 	parser := flags.NewParser(&opts, flags.Default)
-	parser.AddCommand("in", "Clock in", "Clock in to a shift", &clockIn)
-	parser.AddCommand("out", "Clock Out", "Clock out of a shift", &clockOut)
+	parser.AddCommand("clock-in", "Clock in", "Clock in to a shift", &clockIn)
+	parser.AddCommand("clock-out", "Clock Out", "Clock out of a shift", &clockOut)
 	parser.AddCommand("report", "Report", "Print report", &report)
 	parser.AddCommand("status", "Status", "Print current shift", &status)
 
-	_, err := flags.Parse(&opts)
+	cmd := &complete.Command{
+		Flags: map[string]complete.Predictor{
+			"--help":               predict.Nothing,
+			"--install-completion": predict.Nothing,
+		},
+		Sub: map[string]*complete.Command{
+			"clock-in":  nil,
+			"clock-out": nil,
+			"report":    nil,
+			"status":    nil,
+		},
+	}
+	cmd.Complete("work")
+
+	if len(os.Args) == 1 {
+		parser.WriteHelp(os.Stderr)
+		os.Exit(2)
+	}
+
+	_, err := parser.Parse()
 	if err != nil {
-		panic(err)
+		os.Exit(2)
 	}
 
-	if opts.Verbose {
-		fmt.Println("Verbose mode enabled")
-	}
+	command := os.Args[1]
 
-	db, err := sql.Open("sqlite", "./example.db")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS shift (id INTEGER PRIMARY KEY, start TIME, end TIME)`)
-	if err != nil {
-		panic(err)
-	}
-
-	var (
-		end string
-		id  int64
-	)
-
-	switch {
-	case len(os.Args) > 1 && os.Args[1] == "in":
-		{
-			rows, err := db.Query(`SELECT id, end FROM shift ORDER BY end DESC LIMIT 1`)
-			if err != nil {
-				panic(err)
-			}
-			if !rows.Next() {
-				rows.Close()
-				now := time.Now()
-				_, err := db.Exec(`INSERT INTO shift (id, start, end) VALUES (1, ?, ?)`,
-					now.Format(time.UnixDate),
-					now.Add(8*time.Hour).Format(time.UnixDate),
-				)
-				if err != nil {
-					panic(err)
-				}
-			} else {
-				if err = rows.Scan(&id, &end); err != nil {
-					panic(err)
-				}
-				rows.Close()
-				now := time.Now()
-				endTime, err := time.Parse(time.UnixDate, end)
-				if err != nil {
-					panic(err)
-				}
-				if time.Until(endTime) < 0 {
-					_, err := db.Exec(`INSERT INTO shift (id, start, end) VALUES (?, ?, ?)`,
-						id+1,
-						now.Format(time.UnixDate),
-						now.Add(8*time.Hour).Format(time.UnixDate),
-					)
-					if err != nil {
-						panic(err)
-					}
-				}
-			}
-		}
-	case len(os.Args) > 1 && os.Args[1] == "out":
-		{
-			rows, err := db.Query(`SELECT id, end FROM shift ORDER BY end DESC LIMIT 1`)
-			if err != nil {
-				panic(err)
-			}
-			defer rows.Close()
-			if rows.Next() {
-				if err = rows.Scan(&id, &end); err != nil {
-					panic(err)
-				}
-				rows.Close()
-				endTime, err := time.Parse(time.UnixDate, end)
-				if err != nil {
-					panic(err)
-				}
-				if time.Until(endTime) > 0 {
-					_, err := db.Exec(`UPDATE shift SET end=? WHERE id=?`, time.Now().Format(time.UnixDate), id)
-					if err != nil {
-						panic(err)
-					}
-				}
-			}
-		}
-	case len(os.Args) > 1 && os.Args[1] == "report":
-		{
-			_, err := db.Query(`SELECT start, end FROM shift ORDER BY end DESC`)
-			if err != nil {
-				panic(err)
-			}
-		}
-	case len(os.Args) > 1 && os.Args[1] == "status":
-		{
-			rows, err := db.Query(`SELECT end FROM shift ORDER BY end DESC LIMIT 1`)
-			if err != nil {
-				panic(err)
-			}
-			if !rows.Next() {
-				fmt.Println("Not clocked in.")
-			} else {
-				if err = rows.Scan(&end); err != nil {
-					panic(err)
-				}
-				defer rows.Close()
-				endTime, err := time.Parse(time.UnixDate, end)
-				if err != nil {
-					panic(err)
-				}
-				if time.Until(endTime) > 0 {
-					fmt.Printf("Hours left: %v\n", endTime.Format(time.TimeOnly))
-				} else {
-					fmt.Printf("Not clocked in.")
-				}
-			}
-		}
+	switch command {
+	case "clock-in":
+		handleClockIn()
+	case "clock-out":
+		handleClockOut()
+	case "report":
+		handleReport()
+	case "status":
+		handleStatus(status)
 	default:
-		{
-			// print usage
-			os.Exit(2)
-		}
+		parser.WriteHelp(os.Stderr)
+		os.Exit(2)
 	}
 }
