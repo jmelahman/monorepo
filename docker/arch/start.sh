e6763bceff4759049fc3cb4ce639b974e38a5b14
Jamison Lahman <jamison@lahman.dev>
2024-12-14 17:43:51 -0800

    docker buildx bake

--- docker/arch/start.sh
+++ docker/arch/start.sh
@@ -5,6 +5,7 @@ _CONTAINER_NAME="archdev"
 trap 'docker stop ${_CONTAINER_NAME} > /dev/null && docker rm ${_CONTAINER_NAME} > /dev/null' EXIT
 docker run \
   -it \
+  -e "DISPLAY=$DISPLAY" \
   -v "${HOME}:/root" \
   -w "/root" \
   --name ${_CONTAINER_NAME} \
