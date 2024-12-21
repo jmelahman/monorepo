73d36263683f9e056e82b54d93023387450f82f5
Jamison Lahman <jamison@lahman.dev>
2024-12-21 01:05:00 -0800

    fix: download progress and thunder sound

--- main.go
+++ main.go
@@ -26,64 +26,64 @@ var (
 		{
 			name:   "Dawn Soundscape from Big Meadows",
 			credit: "J. Job",
-			url:    "https://www.nps.gov/av/imr/avElement/romo-DawnAmbientROMO6816BigMeadowsFinal1.mp3",
+			url:    "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/romo-DawnAmbientROMO6816BigMeadowsFinal1.mp3",
 		},
 		{
 			name:   "Dawn Soundscape from the Sun Valley Trial",
 			credit: "J. Job",
-			url:    "https://www.nps.gov/av/imr/avElement/romo-DawnAmbientROMO6916SunValleyTrailFinal1.mp3",
+			url:    "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/romo-DawnAmbientROMO6916SunValleyTrailFinal1.mp3",
 		},
 		{
 			// https://www.nps.gov/yell/learn/photosmultimedia/sounds-soundscapes.htm
 			name:   "Lake",
 			credit: "NPS & MSU Acoustic Atlas/Jennifer Jerrett",
-			url:    "https://www.nps.gov/av/imr/avElement/yell-YELLLakeSoundscape20160914T03ms.mp3",
+			url:    "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/yell-YELLLakeSoundscape20160914T03ms.mp3",
 		},
 		{
 			name:   "Lower Geyser Basin (Strong Wind)",
 			credit: "NPS/Peter Comley",
 			// https://www.nps.gov/yell/learn/photosmultimedia/sounds-soundscapes.htm
-			url: "https://www.nps.gov/av/imr/avElement/yell-040201LowerGeyserBasinWindInTreesBinaural01011.mp3",
+			url: "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/yell-040201LowerGeyserBasinWindInTreesBinaural01011.mp3",
 		},
 		{
 			name:   "Old Faithful (Remixed)",
 			credit: "NPS/Jennifer Jerrett and Peter Comley",
 			// https://www.nps.gov/yell/learn/photosmultimedia/sounds-oldfaithful.htm
-			url: "https://www.nps.gov/av/imr/avElement/yell-00150325YellowstoneOldFaithfulGeyserEruption3Mix3Alt101.mp3",
+			url: "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/yell-00150325YellowstoneOldFaithfulGeyserEruption3Mix3Alt101.mp3",
 		},
 		{
 			name:   "Stream Soundscape from the Black Canyon Trail",
 			credit: "J. Job",
 			// https://www.nps.gov/romo/learn/photosmultimedia/sounds-ambient-soundscapes.htm
-			url: "https://www.nps.gov/av/imr/avElement/romo-StreamAmbientROMO52516BlackCanyonTrailFinal1.mp3",
+			url: "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/romo-StreamAmbientROMO52516BlackCanyonTrailFinal1.mp3",
 		},
 		{
 			name:   "Thunder (and American Robin)",
 			credit: "NPS/Jennifer Jerrett",
 			// https://www.nps.gov/yell/learn/photosmultimedia/sounds-thunder.htm
-			url: "https://www.nps.gov/av/imr/avElement/yell-Thunderandbirds140704.mp3",
+			url: "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/yell-Thunderandbirds140704.mp3",
 		},
 		{
 			name:   "Thunderstorm Soundscape from the Black Canyon Trail",
 			credit: "J. Job",
-			url:    "https://www.nps.gov/av/imr/avElement/romo-ThunderstormAmbientROMO52616BlackCanyonTrail1.mp3",
+			url:    "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/romo-ThunderstormAmbientROMO52616BlackCanyonTrail1.mp3",
 		},
 		{
 			name:   "Wind on Peale Island",
 			credit: "NPS & MSU Acoustic Atlas/Jennifer Jerrett",
 			// https://www.nps.gov/yell/learn/photosmultimedia/sounds-soundscapes.htm
-			url: "https://www.nps.gov/av/imr/avElement/yell-YELLCabinSoundsWind20160912T032.mp3",
+			url: "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/yell-YELLCabinSoundsWind20160912T032.mp3",
 		},
 		{
 			name:   "Wind Soundscape from Gem Lake",
 			credit: "J. Job",
-			url:    "https://www.nps.gov/av/imr/avElement/romo-WindAmbientGemLakeROMO52516Final1.mp3",
+			url:    "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/romo-WindAmbientGemLakeROMO52516Final1.mp3",
 		},
 		{
 			name:   "Woodstove",
 			credit: "NPS & MSU Acoustic Atlas",
 			// https://www.nps.gov/yell/learn/photosmultimedia/sounds-soundscapes.htm
-			url: "https://www.nps.gov/av/imr/avElement/yell-YELLPealeCabinWoodstove20160914T15ms.mp3",
+			url: "https://www.nps.gov/nps-audiovideo/legacy/mp3/imr/avElement/yell-YELLPealeCabinWoodstove20160914T15ms.mp3",
 		},
 	}
 )
@@ -115,7 +115,7 @@ func (pr *ProgressReader) Read(p []byte) (int, error) {
 
 	if pr.TotalSize > 0 {
 		percent := float64(pr.DownloadedSize) / float64(pr.TotalSize) * 100
-		fmt.Printf("\rProgress: %.0f%%", percent)
+		fmt.Printf("\rDownloading.. %.0f%%", percent)
 	} else {
 		fmt.Printf("\rDownloading... %.0f MB", float64(pr.DownloadedSize)/(1024*1024))
 	}
