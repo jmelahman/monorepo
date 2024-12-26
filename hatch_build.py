2c1e49d8654e309da598db853746a6326cce09fe
Jamison Lahman <jamison@lahman.dev>
2024-12-26 00:28:22 -0800

    fix version

--- hatch_build.py
+++ hatch_build.py
@@ -8,11 +8,13 @@ class GoBinaryBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         build_data["pure_python"] = False
         binary_name = self.config["binary_name"]
+        tag = os.getenv("GITHUB_REF_NAME", "dev")
+        commit = os.getenv("GITHUB_SHA", "none")
 
         if not os.path.exists(binary_name):
             print(f"Building Go binary '{binary_name}'...")
             subprocess.check_call(
-                ["go", "build",  f"-ldflags=-X main.version={version} -s -w", "-o", binary_name],
+                ["go", "build",  f"-ldflags=-X main.version={tag} -X main.commit={commit} -s -w", "-o", binary_name],
                 env={"GOOS": "linux", "GOARCH": "amd64", **os.environ},
             )
 
