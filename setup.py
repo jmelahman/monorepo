c647e4e61a75557f60e8ec2680b19a8b1f090e3c
Jamison Lahman <jamison@lahman.dev>
2024-12-25 20:42:25 -0800

    migrate setup to pyproject

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import os
 import shutil
 import subprocess
 
-from setuptools import setup
 from setuptools.command.build import build
 from setuptools.command.install import install
 
@@ -27,22 +26,3 @@ class PostInstallCommand(install):
         shutil.move(binary_source, binary_dest)
 
         install.run(self)
-
-setup(
-    name="nature-sounds",
-    packages=[],
-    include_package_data=True,
-    cmdclass={
-        "build": BuildGoBinary,
-        "install": PostInstallCommand,
-    },
-    description="A nature sounds player for the command-line",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Go",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
-)
