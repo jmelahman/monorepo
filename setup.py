73e738b39f9bb6c2119baf9b4f499aaa081652f5
Jamison Lahman <jamison@lahman.dev>
2024-12-25 22:11:03 -0800

    fix packaging

--- setup.py
+++ setup.py
@@ -7,22 +7,24 @@ from setuptools import setup
 from setuptools.command.build import build
 from setuptools.command.install import install
 
+PACKAGE_NAME = "work"
+
 class BuildGoBinary(build):
     def run(self):
-        if not os.path.exists("work"):
+        if not os.path.exists(PACKAGE_NAME):
             print("Building Go binary...")
-            tag = os.environ["GITHUB_REF_NAME"]
-            commit = os.environ["GITHUB_SHA"]
+            tag = os.getenv("GITHUB_REF_NAME", "dev")
+            commit = os.getenv("GITHUB_SHA", "none")
             subprocess.check_call(
-                ["go", "build",  f"-ldflags=-X main.version={tag} -X main.commit={commit} -s -w", "-o", "work", "main.go"],
+                ["go", "build",  f"-ldflags=-X main.version={tag} -X main.commit={commit} -s -w", "-o", PACKAGE_NAME, "main.go"],
                 env={"GOOS": "linux", "GOARCH": "amd64", **os.environ},
             )
         build.run(self)
 
 class PostInstallCommand(install):
     def run(self):
-        binary_source = os.path.join(os.path.dirname(__file__), "work")
-        binary_dest = os.path.join(self.install_scripts, "work")
+        binary_source = os.path.join(os.path.dirname(__file__), PACKAGE_NAME)
+        binary_dest = os.path.join(self.install_scripts, PACKAGE_NAME)
 
         os.makedirs(self.install_scripts, exist_ok=True)
         shutil.move(binary_source, binary_dest)
@@ -30,20 +32,8 @@ class PostInstallCommand(install):
         install.run(self)
 
 setup(
-    name="work-bin",
-    packages=[],
-    include_package_data=True,
     cmdclass={
         "build": BuildGoBinary,
         "install": PostInstallCommand,
     },
-    description="A stupid simple time tracker",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Go",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.6",
 )
