b4150366e7a10851976031e64fb578a7b681fcde
Jamison Lahman <jamison@lahman.dev>
2024-12-27 01:01:50 -0800

    stderr

--- git/git.go
+++ git/git.go
@@ -2,6 +2,7 @@ package git
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -9,32 +10,38 @@ import (
 func GetLatestSemverTag() (string, error) {
 	// Use git describe to get the most recent tag
 	cmd := exec.Command("git", "describe", "--tags", "--abbrev=0")
+	cmd.Stderr = os.Stderr
 	output, err := cmd.Output()
 	if err != nil {
 		return "v0.0.0", nil
 	}
 
 	return strings.TrimSpace(string(output)), nil
+}
 
-	// Fetch all tags for additional context
-	//tagsCmd := exec.Command("git", "tag", "-l", "v[0-9]*.[0-9]*.[0-9]*", "--sort=-v:refname")
-	//tagsOutput, err := tagsCmd.Output()
-	//if err != nil {
-	//	return latestTag, nil, nil
-	//}
+// List all git tags
+func ListTags() ([]string, error) {
+	cmd := exec.Command("git", "tag", "-l", "v[0-9]*.[0-9]*.[0-9]*")
+	cmd.Stderr = os.Stderr
+	tagsOutput, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
 
-	//tagList := strings.Split(strings.TrimSpace(string(tagsOutput)), "\n")
+	tagList := strings.Split(strings.TrimSpace(string(tagsOutput)), "\n")
 
-	//return latestTag, tagList, nil
+	return tagList, nil
 }
 
 func CreateAndPushTag(tag string, remote string) error {
 	cmd := exec.Command("git", "tag", tag)
+	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to create tag: %w", err)
 	}
 
-	cmd = exec.Command("git", "push", remote, tag)
+	cmd = exec.Command("git", "push", "--quiet", remote, tag)
+	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to push tag to %s: %w", remote, err)
 	}
@@ -43,7 +50,8 @@ func CreateAndPushTag(tag string, remote string) error {
 }
 
 func FetchSemverTags(remote string) error {
-	cmd := exec.Command("git", "fetch", "--prune", remote, "refs/tags/v*:refs/tags/v*")
+	cmd := exec.Command("git", "fetch", "--quiet", "--prune", remote, "refs/tags/v*:refs/tags/v*")
+	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to fetch tags from %s: %w", remote, err)
 	}
@@ -52,6 +60,7 @@ func FetchSemverTags(remote string) error {
 
 func IsHEADAlreadyTagged() (bool, error) {
 	cmd := exec.Command("git", "tag", "--points-at", "HEAD")
+	cmd.Stderr = os.Stderr
 	output, err := cmd.Output()
 	if err != nil {
 		return false, fmt.Errorf("failed to check tags for HEAD: %w", err)
