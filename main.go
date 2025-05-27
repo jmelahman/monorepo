2bd8f3873b351a55dc3655c7401e55105228790a
Jamison Lahman <jamison@lahman.dev>
2025-05-26 19:05:12 -0700

    rm unused

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
 	"time"
 
 	"github.com/eiannone/keyboard"
@@ -25,59 +22,6 @@ import (
 var (
 	version = "dev"
 )
-
-type ProgressReader struct {
-	Reader         io.Reader
-	TotalSize      int64
-	DownloadedSize int64
-}
-
-func (pr *ProgressReader) Read(p []byte) (int, error) {
-	n, err := pr.Reader.Read(p)
-	pr.DownloadedSize += int64(n)
-
-	if pr.TotalSize > 0 {
-		percent := float64(pr.DownloadedSize) / float64(pr.TotalSize) * 100
-		fmt.Printf("\rDownloading.. %.0f%%", percent)
-	} else {
-		fmt.Printf("\rDownloading... %.0f MB", float64(pr.DownloadedSize)/(1024*1024))
-	}
-
-	return n, err
-}
-
-func downloadFileWithProgress(url, filepath string) error {
-	out, err := os.Create(filepath)
-	if err != nil {
-		return fmt.Errorf("failed to create file: %w", err)
-	}
-	defer out.Close()
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Errorf("failed to make request: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("bad status: %s", resp.Status)
-	}
-
-	contentLength, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
-
-	progressReader := &ProgressReader{
-		Reader:         resp.Body,
-		TotalSize:      int64(contentLength),
-		DownloadedSize: 0,
-	}
-
-	_, err = io.Copy(out, progressReader)
-	if err != nil {
-		return fmt.Errorf("failed to write to file: %w", err)
-	}
-
-	return nil
-}
 
 func ListPicker(items []sounds.Sound) (int, error) {
 	screen, err := tcell.NewScreen()
