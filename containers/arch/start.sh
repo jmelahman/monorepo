e5b7c664d81ddfc0e943f43eb447fbc643c89ce5
Jamison Lahman <jamison@lahman.dev>
2025-06-15 16:40:58 -0700

    chore: fix and run shfmt

--- containers/arch/start.sh
+++ containers/arch/start.sh
@@ -4,10 +4,10 @@ _CONTAINER_NAME="archdev"
 
 trap 'docker stop ${_CONTAINER_NAME} > /dev/null && docker rm ${_CONTAINER_NAME} > /dev/null' EXIT
 docker run \
-  -it \
-  -e "DISPLAY=$DISPLAY" \
-  -v "${HOME}:/root" \
-  -w "/root" \
-  --name ${_CONTAINER_NAME} \
-  lahmanja/arch:latest \
-  /usr/bin/bash
+	-it \
+	-e "DISPLAY=$DISPLAY" \
+	-v "${HOME}:/root" \
+	-w "/root" \
+	--name ${_CONTAINER_NAME} \
+	lahmanja/arch:latest \
+	/usr/bin/bash
