3d3c406828f6b183a53d81842d4242f0706d4542
Jamison Lahman <jamison@lahman.dev>
2025-06-15 17:38:52 -0700

    ci: fix ruff {check,format}

--- go-bin/hatch_build.py
+++ go-bin/hatch_build.py
@@ -1,30 +1,33 @@
+from __future__ import annotations
+
 import os
 import re
 import shutil
-import urllib.request
 import tarfile
 import tempfile
+import urllib.request
 import zipfile
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
 import manygo
 
 
 class GoBinaryBuildHook(BuildHookInterface):
-    def initialize(self, version, build_data):
+    def initialize(self, version, build_data) -> None:  # noqa: ANN001
         build_data["pure_python"] = False
         goos = os.getenv("GOOS")
         goarch = os.getenv("GOARCH")
         if goos and goarch:
             build_data["tag"] = "py3-none-" + manygo.get_platform_tag(goos=goos, goarch=goarch)
         tag = os.environ["GITHUB_REF_NAME"]
-        match = re.search(r'v(\d+\.\d+\.\d+)(?:\.\d+)?', tag)
+        match = re.search(r"v(\d+\.\d+\.\d+)(?:\.\d+)?", tag)
         assert match is not None
         version = match.group(1)
         if goos == "windows":
-            archive = "go{}.{}-{}.zip".format(version, goos, goarch)
+            archive = f"go{version}.{goos}-{goarch}.zip"
         else:
-            archive = "go{}.{}-{}.tar.gz".format(version, goos, goarch)
+            archive = f"go{version}.{goos}-{goarch}.tar.gz"
 
         if not os.path.exists(archive):
             urllib.request.urlretrieve("https://storage.googleapis.com/golang/" + archive, archive)
@@ -32,11 +35,11 @@ class GoBinaryBuildHook(BuildHookInterface):
         if not os.path.exists("go"):
             with tempfile.TemporaryDirectory() as temp_dir:
                 if goos == "windows":
-                    with zipfile.ZipFile(archive) as zip:
-                        zip.extractall(path=temp_dir)
+                    with zipfile.ZipFile(archive) as zip:  # noqa: A001
+                        zip.extractall(path=temp_dir)  # noqa: S202
                 else:
                     with tarfile.open(archive, "r:gz") as tar:
-                        tar.extractall(path=temp_dir)
+                        tar.extractall(path=temp_dir)  # noqa: S202
                 shutil.move(os.path.join(temp_dir, "go"), self.root)
 
         build_data["force_include"] = {
--- go-bin/src/go/__init__.py
+++ go-bin/src/go/__init__.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 import os
 import sys
 
-def main():
+
+def main() -> None:
     goroot = os.path.abspath(os.path.dirname(__file__))
     go_bin = os.path.join(goroot, "bin", "go")
     os.environ["GOROOT"] = goroot
-    os.execv(go_bin, [go_bin] + sys.argv[1:])
+    os.execv(go_bin, [go_bin] + sys.argv[1:])  # noqa: S606
