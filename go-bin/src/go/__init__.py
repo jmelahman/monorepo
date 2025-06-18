43147db1eaa76f8d00ff1677852e08c9852f23b3
Jamison Lahman <jamison@lahman.dev>
2025-06-17 23:37:24 -0700

    ci: fix go-bin lint

--- go-bin/src/go/__init__.py
+++ go-bin/src/go/__init__.py
@@ -8,4 +8,4 @@ def main() -> None:
     goroot = os.path.abspath(os.path.dirname(__file__))
     go_bin = os.path.join(goroot, "bin", "go")
     os.environ["GOROOT"] = goroot
-    os.execv(go_bin, [go_bin] + sys.argv[1:])  # noqa: S606
+    os.execv(go_bin, [go_bin, *sys.argv[1:]])  # noqa: S606
