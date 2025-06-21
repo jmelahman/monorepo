103335172c684b50f9129ff4c08266299e921b91
Jamison Lahman <jamison@lahman.dev>
2025-06-21 00:04:37 -0700

    feat: docker-status widget

--- dashboard/main.go
+++ dashboard/main.go
@@ -10,11 +10,13 @@ func main() {
 
 	// Create the credits widget
 	creditsWidget := widgets.NewCreditsWidget()
+	dockerWidget := widgets.NewDockerWidget()
 
 	// Wrap the widget in a flex layout to center it
 	flex := tview.NewFlex().
 		AddItem(nil, 0, 1, false).
 		AddItem(creditsWidget, 30, 1, false).
+		AddItem(dockerWidget, 31, 1, false).
 		AddItem(nil, 0, 1, false)
 
 	if err := app.SetRoot(flex, true).Run(); err != nil {
