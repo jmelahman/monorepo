ce6af24fefba53f2fc8741821e00a54511f49a7f
Jamison Lahman <jamison@lahman.dev>
2025-07-22 22:56:50 -0700

    sam harris feed

--- website/scripts/feed.js
+++ website/scripts/feed.js
@@ -1,6 +1,7 @@
 const feeds = [
   { title: "Thorsten Ball", url: "https://registerspill.thorstenball.com/feed" },
   { title: "Kyla Scanlon", url: "https://kyla.substack.com/feed" },
+  { title: "Sam Harris", url: "http://wakingup.libsyn.com/rss" },
 ];
 
 const proxy = 'https://corsproxy.io/?';
