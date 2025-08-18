4d9e736cf9878950006f42d863fd986deee03fce
Jamison Lahman <jamison@lahman.dev>
2025-08-18 01:58:33 -0700

    document variables

--- connections-ssh/terraform/variables.tf
+++ connections-ssh/terraform/variables.tf
@@ -1,5 +1,24 @@
-variable "tenancy_ocid" {}
-variable "compartment_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "region" {}
+variable "tenancy_ocid" {
+  type        = string
+  description = "The OCID of the tenancy"
+}
+
+variable "compartment_ocid" {
+  type        = string
+  description = "The OCID of the compartment"
+}
+
+variable "user_ocid" {
+  type        = string
+  description = "The OCID of the user"
+}
+
+variable "fingerprint" {
+  type        = string
+  description = "The fingerprint of the API key"
+}
+
+variable "region" {
+  type        = string
+  description = "The region where resources will be created"
+}
