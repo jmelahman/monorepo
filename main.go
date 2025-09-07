871361d27b6a2be4bfd84bb0f9db764833a33fe1
Jamison Lahman <jamison@lahman.dev>
2025-09-06 18:07:34 -0700

    Squashed 'connections/' changes from c0929e9..202d033
    202d033 fix panic on exit
    1728d1b remove unnecessary styling on click
    a16ae6b improve 16 bit color accessibility
    2d4cac7 refactor focus and styling
    d055027 fix styling for armv6 w/ xterm256color (#21)
    fd04398 go mod tidy
    d2a0a50 upgrade go deps
    edfd985 upgrade workflows
    
    git-subtree-dir: connections
    git-subtree-split: 202d0330b0c6863bbf36dd39edc6b3b3aacb9d99

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/jmelahman/connections/game"
 
 	"github.com/rivo/tview"
 )
 
 func main() {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		panic(err)
+	}
+
 	app := tview.NewApplication()
-	if err := game.Run(app); err != nil {
+	if err := game.Run(app, screen); err != nil {
 		panic(err)
 	}
 }
