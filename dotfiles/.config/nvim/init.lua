cc88e7ec2a3ceaa00971d9f0e5fee591d7bb12e8
Jamison Lahman <jamison@lahman.dev>
2026-09-25 04:25:42 -0700

    upgrade `none-ls`

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -162,7 +162,7 @@ require("lazy").setup({
             }),
           },
           on_attach = function(client, bufnr)
-            if client.supports_method("textDocument/formatting") then
+            if client:supports_method("textDocument/formatting", bufnr) then
               vim.api.nvim_create_autocmd("BufWritePre", {
                 buffer = bufnr,
                 callback = function()
